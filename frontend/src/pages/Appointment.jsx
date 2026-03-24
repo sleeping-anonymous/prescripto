c98cb79ffd121ce16d908c91a367674430eb9114
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-03-24 19:14:50 +0530

    Appointment and Rel Doctors page

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -1,12 +1,14 @@
-import React, { use, useContext, useEffect, useState } from 'react'
+import React, {  useContext, useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 import { AppContext } from '../context/AppContext';
 import { assets } from '../assets/assets_frontend/assets';
+import RelatedDoctors from '../components/RelatedDoctors';
 
 const Appointment = () => {
 
   const {docId} = useParams()
   const {doctors,currencySymbol} = useContext(AppContext)
+  const daysOfWeek = ['SUN','MON','TUE','WED','THU','FRI','SAT']
 
   const [docInfo,setDocInfo] = useState(null);
   const [docSlots,setDocSlots] = useState([])
@@ -50,19 +52,19 @@ const Appointment = () => {
 
       while(currentDate < endTime){
         let formattedTime = currentDate.toLocaleTimeString([],{hour:'2-digit' , minute : '2-digit'})
+        
+        //add slot to array
+        timeSlots.push({
+          datetime : new Date(currentDate),
+          time: formattedTime
+        })
+        
+        // Increment curr time by 30 mins
+        currentDate.setMinutes(currentDate.getMinutes() + 30)
       }
-
-      //add slot to array
-      timeSlots.push({
-        datetime : new Date(currentDate),
-        time: formattedTime
-      })
-
-      // Increment curr time by 30 mins
-      currentDate.setMinutes(currentDate.getMinutes() + 30)
+      
+      setDocSlots(prev => ([...prev,timeSlots]))
     }
-
-    setDocSlots(prev => ([...prev,timeSlots]))
   }
 
   useEffect(() => {
@@ -108,6 +110,35 @@ const Appointment = () => {
           </p>
           </div>
         </div>
+
+        {/* -----Booking SLots ------------*/}
+        <div className='sm:ml-72 sm:pl-4 mt-4 font-medium text-gray-700'>
+          <p>Booking Slots</p>
+          <div className='flex gap-3 items-center w-full overflow-x scroll mt-4'>
+            {
+              docSlots.length && docSlots.map((item,index) => (
+                <div onClick={() => setSlotIndex(index)} className={`text-center py-6 min-w-16 rounded-full cursor-pointer ${slotIndex === index ?"bg-primary text-white" : "border border-gray-200"}`} key = {index}>
+                   <p>{item[0] && daysOfWeek[item[0].datetime.getDay()]}</p>
+                   <p>{item[0] && item[0].datetime.getDate()}</p>
+                </div>
+              ))
+            }
+          </div>
+
+          <div className='flex items-center gap-3 w-full overflow-x-scroll mt-4'>
+            {docSlots.length && docSlots[slotIndex].map((item,index)=> (
+                <p onClick={() => setSlotTime(item.time)} className={`text-sm font-light flex-shrink-0 px-5 py-2 rounded-full cursor pointer ${item.time === slotTime ? 'bg-primary text-white' : 'text-gray-400 border border-gray-300'}`} key={index}>
+                  {item.time.toLowerCase()}
+                </p>
+            ))}
+          </div>
+
+          <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full my-6'>Book an Appointment</button>
+        </div>
+
+        {/* Listing Related Doctors */}
+
+        <RelatedDoctors docId={docId} speciality={docInfo.speciality} />
     </div>
   )
 }
--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -4,6 +4,10 @@ const Contact = () => {
   return (
     <div>
 
+      <div className='text-center text-2xl pt-10 text-gray-500'>
+        <p>CONTACT <span className='text-gray-700 font-semibold'> US </span></p>
+      </div>
+
     </div>
   )
 }
