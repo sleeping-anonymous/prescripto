09ecb311b1924a59ea08e3c7fcb9d797017eb05f
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-04-04 17:37:53 +0530

    User profile and editing it

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -8,15 +8,14 @@ const AppContextProvider = (props) => {
 
     const currencySymbol = '$'
     const backendUrl = import.meta.env.VITE_BACKEND_URL
+
     const [doctors,setDoctors] = useState([])
+    const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token') : false)    
+    const [userData , setUserData] = useState(false)
 
-    const value = {
-        doctors,
-        currencySymbol
-    }
-
+    
     const getDoctorsData = async () => {
-       
+        
         try {
             
             const {data} = await axios.get(backendUrl + '/api/doctor/list')
@@ -25,7 +24,25 @@ const AppContextProvider = (props) => {
             } else {
                 toast.error(data.message)
             }
+            
+            
+        } catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
 
+    const loadUserProfileData = async () => {
+
+        try {
+            
+            const {data} = await axios.get(backendUrl + '/api/user/get-profile' , {headers : {token}})
+            if(data.success){
+                setUserData(data.userData)
+
+            } else{
+                toast.error(data.message)
+            }
 
         } catch (error) {
             console.log(error)
@@ -33,9 +50,27 @@ const AppContextProvider = (props) => {
         }
     }
 
+    
+    const value = {
+        doctors,
+        currencySymbol,
+        token, setToken,
+        backendUrl ,
+        userData , setUserData, 
+        loadUserProfileData
+    }
+
     useEffect(() => {
         getDoctorsData()
     } , [])
+    
+    useEffect(() => {
+       if(token){
+            loadUserProfileData()
+        } else {
+            setUserData(false)
+        }
+    } , [token])
 
     return(
         <AppContext.Provider value={value}>
