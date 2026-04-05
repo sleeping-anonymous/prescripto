7651011311a42b1b4d2b2dde55e096ae3cd7d723
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-04-06 00:23:56 +0530

    Integrating payments gateway using razorpay

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -5,6 +5,7 @@ import jwt from 'jsonwebtoken'
 import {v2 as cloudinary} from 'cloudinary'
 import doctorModel from '../models/doctorModel.js'
 import appointmentModel from '../models/appointmentModel.js'
+import razorpay from 'razorpay'
 
 
 // API to register user 
@@ -245,4 +246,63 @@ const cancelAppointment = async (req ,res ) => {
     }
 }
 
-export {registerUser , loginUser , getProfile , updateProfile , bookAppointment , listAppointment , cancelAppointment}
+const razorpayInstance = new razorpay({
+    key_id : process.env.RAZORPAY_KEY_ID,
+    key_secret : process.env.RAZORPAY_KEY_SECRET
+})
+
+// API TO MAKE PAYMENT OF APPOINTMENT USING RAZORPAY
+const paymentRazorpay = async (req , res) => {
+
+    try {
+        
+        const { appointmentId } = req.body
+        const appointmentData = await appointmentModel.findById(appointmentId)
+
+        if(!appointmentData || appointmentData.cancelled ){
+            return res.json({success:false, message : "Appointment Cancelled or not found"})
+        }
+
+        // creating options for razorpay payment
+        const options = {
+            amount : appointmentData.amount * 100 , 
+            currency : process.env.CURRENCY ,
+            receipt : appointmentId 
+        }
+
+        // creation of an order
+        const order = await razorpayInstance.orders.create(options)
+
+        res.json({success : true , order})
+
+    } catch (error) {
+        console.log(error);
+        res.json({success : false,message : error.message})
+    }
+    
+}
+
+// API TO VERIFY PAYMENT OF RAZORPAY 
+const verifyRazorpay = async (req, res) => {
+    try {
+        
+        const { razorpay_order_id } = req.body
+        const orderInfo = await razorpayInstance.orders.fetch(razorpay_order_id)
+
+        if(orderInfo.status === 'paid'){
+            await appointmentModel.findByIdAndUpdate(orderInfo.receipt, {payment : true})
+            res.json({success: true , message : "Payment Successful"})
+        } else {
+            res.json({success: false , message : "Payment Failed"})
+
+        }
+
+        
+
+    } catch (error) {
+        console.log(error);
+        res.json({success : false,message : error.message})
+    }
+}   
+
+export {registerUser , loginUser , getProfile , updateProfile , bookAppointment , listAppointment , cancelAppointment , paymentRazorpay , verifyRazorpay}
--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -2,6 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import {AppContext} from '../context/AppContext'
 import axios from 'axios';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 const MyAppointments = () => {
 
@@ -9,6 +10,8 @@ const MyAppointments = () => {
 
   const [appointments , setAppointments ] = useState([])
   const months = [" " , "Jan" , "Feb" , "Mar" ,"Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"]   // 1st is empty sincce we send month with index 1 starting
+
+  const navigate = useNavigate()
 
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split('_')
@@ -53,6 +56,59 @@ const MyAppointments = () => {
     }
   }
 
+  const initPay = (order) => {   
+
+    const options = {
+      key : import.meta.env.VITE_RAZORPAY_KEY_ID , 
+      amount : order.amount,
+      currency : order.currency , 
+      name : 'Appointment Payment',
+      description : 'Appointment Payment' ,
+      order_id : order.id , 
+      receipt : order.receipt,
+      handler : async (response) => {
+        console.log(response)
+        
+        try {
+          
+          const { data } = await axios.post(backendUrl + '/api/user/verifyRazorpay' , response , {headers : {token}})
+          
+          if(data.success){
+
+            getUserAppointments()
+            navigate('/my-appointments')
+          } 
+
+        } catch (error) {
+          console.log(error)
+          toast.error(error.message)
+          
+        }
+
+      }
+ 
+    }
+
+    const rzp = new window.Razorpay(options)
+    rzp.open()
+
+  }
+
+  const appointmentRazorpay = async ( appointmentId ) => {
+    
+    try {
+      
+      const { data } = await axios.post(backendUrl + '/api/user/payment-razorpay' , {appointmentId} , {headers : {token}})
+
+      if(data.success){
+        initPay(data.order)
+      }
+
+    } catch (error) {
+      
+    }
+  }
+
   useEffect(() => {
 
     if(token){
@@ -81,10 +137,12 @@ const MyAppointments = () => {
               <div></div> 
               {/* empty div to keep btns to right in mobile view */}
               <div className='flex flex-col gap-2 justify-end'>
-                {!item.cancelled && <button className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300 '>Pay Online</button> }
+                {!item.cancelled && item.payment && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
+                {!item.cancelled && !item.payment &&  <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300 '>Pay Online</button> }
                 { !item.cancelled &&  <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300 '>Cancel Appointment</button>}
 
-                { item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>}
+                { item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>}  *
+                {/* the appointment can be cancelled even after paid , so how does money work , does it have to be implemented ??  */}
               </div>
             </div>
           ))}
