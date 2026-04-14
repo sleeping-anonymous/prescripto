7717eadfbdeaff9e02a9cb192e25bce883eb1883
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-04-15 00:21:41 +0530

    Fixed the Completed button logic

--- frontend/src/pages/MyAppointments.jsx
+++ frontend/src/pages/MyAppointments.jsx
@@ -1,18 +1,18 @@
 import React, { useContext, useEffect, useState } from 'react'
-import {AppContext} from '../context/AppContext'
+import { AppContext } from '../context/AppContext'
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import { useNavigate } from 'react-router-dom';
 
 const MyAppointments = () => {
 
-  const { backendUrl , token , getDoctorsData } = useContext(AppContext);
+  const { backendUrl, token, getDoctorsData } = useContext(AppContext);
 
-  const [appointments , setAppointments ] = useState([])
-  const months = [" " , "Jan" , "Feb" , "Mar" ,"Apr" , "May" , "Jun" , "Jul" , "Aug" , "Sep" , "Oct" , "Nov" , "Dec"]   // 1st is empty sincce we send month with index 1 starting
-  
+  const [appointments, setAppointments] = useState([])
+  const months = [" ", "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]   // 1st is empty sincce we send month with index 1 starting
+
   const navigate = useNavigate()
-  
+
   const slotDateFormat = (slotDate) => {
     const dateArray = slotDate.split('_')
     return dateArray[0] + " " + months[Number(dateArray[1])] + " " + dateArray[2]
@@ -22,31 +22,31 @@ const MyAppointments = () => {
 
     try {
 
-      const { data } = await axios.get(backendUrl + '/api/user/appointments' , {headers : {token}})
+      const { data } = await axios.get(backendUrl + '/api/user/appointments', { headers: { token } })
 
-      if(data.success){
+      if (data.success) {
         setAppointments(data.appointments.reverse())  // to get latest appointment on top used revers
         console.log(data.appointments);
-        
+
       }
 
     } catch (error) {
       console.log(error);
       toast.error(error.message)
-      
+
     }
   }
 
   const cancelAppointment = async (appointmentId) => {
 
     try {
-      
-      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment' , {appointmentId} , {headers : {token}})    // appointmentId passed in req body
-      if(data.success){
+
+      const { data } = await axios.post(backendUrl + '/api/user/cancel-appointment', { appointmentId }, { headers: { token } })    // appointmentId passed in req body
+      if (data.success) {
         toast.success(data.message)
         getUserAppointments()
         getDoctorsData()     // so that we dont need to reload the page to get canceeled time slot also visible
-       }  else {
+      } else {
         toast.error(data.message)
       }
 
@@ -56,37 +56,37 @@ const MyAppointments = () => {
     }
   }
 
-  const initPay = (order) => {   
+  const initPay = (order) => {
 
     const options = {
-      key : import.meta.env.VITE_RAZORPAY_KEY_ID , 
-      amount : order.amount,
-      currency : order.currency , 
-      name : 'Appointment Payment',
-      description : 'Appointment Payment' ,
-      order_id : order.id , 
-      receipt : order.receipt,
-      handler : async (response) => {
+      key: import.meta.env.VITE_RAZORPAY_KEY_ID,
+      amount: order.amount,
+      currency: order.currency,
+      name: 'Appointment Payment',
+      description: 'Appointment Payment',
+      order_id: order.id,
+      receipt: order.receipt,
+      handler: async (response) => {
         console.log(response)
-        
+
         try {
-          
-          const { data } = await axios.post(backendUrl + '/api/user/verifyRazorpay' , response , {headers : {token}})
-          
-          if(data.success){
+
+          const { data } = await axios.post(backendUrl + '/api/user/verifyRazorpay', response, { headers: { token } })
+
+          if (data.success) {
 
             getUserAppointments()
             navigate('/my-appointments')
-          } 
+          }
 
         } catch (error) {
           console.log(error)
           toast.error(error.message)
-          
+
         }
 
       }
- 
+
     }
 
     const rzp = new window.Razorpay(options)
@@ -94,59 +94,60 @@ const MyAppointments = () => {
 
   }
 
-  const appointmentRazorpay = async ( appointmentId ) => {
-    
-    try {
-      
-      const { data } = await axios.post(backendUrl + '/api/user/payment-razorpay' , {appointmentId} , {headers : {token}})
+  const appointmentRazorpay = async (appointmentId) => {
 
-      if(data.success){
+    try {
+
+      const { data } = await axios.post(backendUrl + '/api/user/payment-razorpay', { appointmentId }, { headers: { token } })
+
+      if (data.success) {
         initPay(data.order)
       }
 
     } catch (error) {
-      
+
     }
   }
 
   useEffect(() => {
 
-    if(token){
+    if (token) {
       getUserAppointments()
     }
 
-  } , [token])
+  }, [token])
 
   return (
     <div>
-        <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointments </p>
-        <div>
-          {appointments.map((item,index)=> (
-            <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
-              <div>
-                <img className='w-32 bg-indigo-50' src={item.docData.image} alt="" />
-              </div>
-              <div className='flex-1 text-sm text-zinc-600'>
-                <p className='text-neutral font-semibold'>{item.docData.name}</p>
-                <p>{item.docData.speciality}</p>
-                <p className='text-zinc-700 font-medium mt-1'>Address:</p>
-                <p className='text-xs'>{item.docData.address.line1}</p>
-                <p className='text-xs'>{item.docData.address.line2}</p>
-                <p className='text-xs mt-1'> <span className='text-sm text-neutral-700 font=medium'>Date & Time :</span> {slotDateFormat(item.slotDate)} | {item.slotTime} </p>
-              </div>
-              <div></div> 
-              {/* empty div to keep btns to right in mobile view */}
-              <div className='flex flex-col gap-2 justify-end'>
-                {!item.cancelled && item.payment && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
-                {!item.cancelled && !item.payment &&  <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300 '>Pay Online</button> }
-                { !item.cancelled &&  <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300 '>Cancel Appointment</button>}
-
-                { item.cancelled && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>}  
-                {/* the appointment can be cancelled even after paid , so how does money work , does it have to be implemented ??  */}
-              </div>
+      <p className='pb-3 mt-12 font-medium text-zinc-700 border-b'>My Appointments </p>
+      <div>
+        {appointments.map((item, index) => (
+          <div className='grid grid-cols-[1fr_2fr] gap-4 sm:flex sm:gap-6 py-2 border-b' key={index}>
+            <div>
+              <img className='w-32 bg-indigo-50' src={item.docData.image} alt="" />
             </div>
-          ))}
-        </div>
+            <div className='flex-1 text-sm text-zinc-600'>
+              <p className='text-neutral font-semibold'>{item.docData.name}</p>
+              <p>{item.docData.speciality}</p>
+              <p className='text-zinc-700 font-medium mt-1'>Address:</p>
+              <p className='text-xs'>{item.docData.address.line1}</p>
+              <p className='text-xs'>{item.docData.address.line2}</p>
+              <p className='text-xs mt-1'> <span className='text-sm text-neutral-700 font=medium'>Date & Time :</span> {slotDateFormat(item.slotDate)} | {item.slotTime} </p>
+            </div>
+            <div></div>
+            {/* empty div to keep btns to right in mobile view */}
+            <div className='flex flex-col gap-2 justify-end'>
+              {!item.cancelled && item.payment && !item.isCompleted && <button className='sm:min-w-48 py-2 border rounded text-stone-500 bg-indigo-50'>Paid</button>}
+              {!item.cancelled && !item.payment && !item.isCompleted && <button onClick={() => appointmentRazorpay(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-primary hover:text-white transition-all duration-300 '>Pay Online</button>}
+              {!item.cancelled && !item.isCompleted && <button onClick={() => cancelAppointment(item._id)} className='text-sm text-stone-500 text-center sm:min-w-48 py-2 border hover:bg-red-600 hover:text-white transition-all duration-300 '>Cancel Appointment</button>}
+
+              {item.cancelled && !item.isCompleted && <button className='sm:min-w-48 py-2 border border-red-500 rounded text-red-500 '>Appointment Cancelled</button>}
+              {/* the appointment can be cancelled even after paid , so how does money work , does it have to be implemented ??  */}
+              {item.isCompleted && <button className='sm:min-w-48 py-2 border border-green-500 rounded text-green-500'>Completed</button>}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
