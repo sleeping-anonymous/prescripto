c2a9e875f800fe415801380e5085ffa8867363d2
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-04-14 23:08:59 +0530

    Doctor dashboard

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -13,6 +13,7 @@ const DoctorContextProvider = (props) => {
     const [dToken,setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : '')
 
     const [appointments,setAppointments] = useState([])
+    const [dashData, setDashData] = useState(false)
 
     const getAppointments = async () => {
         try {
@@ -65,12 +66,32 @@ const DoctorContextProvider = (props) => {
         }
     }
 
+    const getDashData = async () => {
+        try {
+            
+            const { data } = await axios.get(backendUrl + '/api/doctor/dashboard' , {headers:{dToken}})
+            if(data.success){
+                setDashData(data.dashData)
+                console.log(data.dashData);
+                
+            } else {
+                toast.error(data.message)
+            }
+
+        } catch (error) {
+            console.log(error);
+            toast.error(error.message)
+        }
+    }
+
     const value = {
         dToken , setDToken,
         backendUrl , 
         appointments, setAppointments , 
         getAppointments,
-        completeAppointment , cancelAppointment
+        completeAppointment , cancelAppointment , 
+        dashData ,  setDashData ,
+        getDashData
     }
 
     return (
