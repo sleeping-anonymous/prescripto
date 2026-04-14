7769e277b1a0d296a352447b0a47946e6de036aa
Monaal Sharma <157833279+sleeping-anonymous@users.noreply.github.com>
2026-04-15 00:44:03 +0530

    Changes in UI such as doc availability

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,41 +12,43 @@ import { DoctorContext } from './context/DoctorContext.jsx';
 import DoctorDashboard from './pages/Doctor/DoctorDashboard.jsx';
 import DoctorAppointments from './pages/Doctor/DoctorAppointments.jsx';
 import DoctorProfile from './pages/Doctor/DoctorProfile.jsx';
+import DoctorsList from './pages/Admin/DoctorsList.jsx';
 
 
 const App = () => {
 
-  const {aToken} = useContext(AdminContext)
-  const {dToken} = useContext(DoctorContext)
+  const { aToken } = useContext(AdminContext)
+  const { dToken } = useContext(DoctorContext)
 
   return aToken || dToken ? (
     <div className='bg-[#F8F9FD]'>
-      <ToastContainer/>
-      <Navbar/>
+      <ToastContainer />
+      <Navbar />
       <div className='flex items-start'>
-        <Sidebar/>
+        <Sidebar />
         <Routes>
           {/* Admin ROute */}
-          <Route path = '/' element = {<></>} />
-          <Route path = '/admin-dashboard' element = {<Dashboard />} />
-          <Route path = '/all-appointments' element = {<AllAppointments />} />
-          <Route path = '/add-doctor' element = {<AddDoctor />} />
-          
+          <Route path='/' element={<></>} />
+          <Route path='/admin-dashboard' element={<Dashboard />} />
+          <Route path='/all-appointments' element={<AllAppointments />} />
+          <Route path='/add-doctor' element={<AddDoctor />} />
+          <Route path='/doctor-list' element={<DoctorsList />} />
+
           {/* Doctor Route */}
-          <Route path = '/doctor-dashboard' element = {<DoctorDashboard />} />
-          <Route path = '/doctor-appointments' element = {<DoctorAppointments />} />
-          <Route path = '/doctor-profile' element = {<DoctorProfile />} />
+          <Route path='/doctor-dashboard' element={<DoctorDashboard />} />
+          <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+          <Route path='/doctor-profile' element={<DoctorProfile />} />
 
         </Routes>
       </div>
     </div>
-  ) 
-  : (
-    <>
-    <Login />
-      <ToastContainer/>
-    </>
   )
+    : (
+      <>
+        <Login />
+        <ToastContainer />
+      </>
+    )
 }
 
 export default App
--- admin/src/pages/Admin/Dashboard.jsx
+++ admin/src/pages/Admin/Dashboard.jsx
@@ -7,17 +7,17 @@ import { AppContext } from '../../context/AppContext'
 
 const Dashboard = () => {
 
-  const {aToken , getDashData , cancelAppointment , dashData} = useContext(AdminContext)
+  const { aToken, getDashData, cancelAppointment, dashData } = useContext(AdminContext)
 
-  const {slotDateFormat} = useContext(AppContext)
+  const { slotDateFormat } = useContext(AppContext)
 
   useEffect(() => {
-    if(aToken){
+    if (aToken) {
       getDashData()
     }
-  } , [aToken])
+  }, [aToken])
 
-  return dashData &&  (    
+  return dashData && (
     <div className='m-5'>
 
       <div className='flex flex-wrap gap-3'>
@@ -49,7 +49,7 @@ const Dashboard = () => {
       </div>
 
       <div className='bg-white'>
-       
+
 
         <div className='flex items-center gap-2.5 px-4 py-4 mt-10 rounded-t border'>
           <img src={assets.list_icon} alt="" />
@@ -58,25 +58,27 @@ const Dashboard = () => {
 
         <div className='pt-4 border border-t-0'>
           {
-            dashData.latestAppointments.map((item,index) => (
-              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key = {index}>
+            dashData.latestAppointments.map((item, index) => (
+              <div className='flex items-center px-6 py-3 gap-3 hover:bg-gray-100' key={index}>
                 <img className='rounded-full w-10' src={item.docData.image} alt="" />
                 <div className='flex-1 text-sm'>
                   <p className='text-gray-800 font-medium'>{item.docData.name}</p>
                   <p className='text-gray-600'>{slotDateFormat(item.slotDate)}</p>
                 </div>
                 {
-                item.cancelled
-                ? <p className='text-red-400 text-xs font-medium'> Cancelled </p>
-                : <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
+                  item.cancelled
+                    ? <p className='text-red-400 text-xs font-medium'> Cancelled </p>
+                    : item.isCompleted
+                      ? <p className='text-green-500 text-xs font-medium'> Completed </p>
+                      : <img onClick={() => cancelAppointment(item._id)} className='w-10 cursor-pointer' src={assets.cancel_icon} alt="" />
                 }
               </div>
             ))
           }
         </div>
 
-      </div> 
-      
+      </div>
+
     </div>
   )
 }
